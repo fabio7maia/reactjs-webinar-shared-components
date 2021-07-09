540426f3f2b8c998d576d3514043a9cb50da1a31
Fábio Maia <fabiomaia@suportes-MacBook-Pro.local>
2021-07-09 22:28:53 +0100

    Refined scaffold package

---
+++ src/packages/_new_package_scaffold_/README.md
@@ -0,0 +1,2 @@
+
+# New Package Scaffold
---
+++ src/packages/_new_package_scaffold_/src/components/example/__stories__/example.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Example } from '..';
+
+export default {
+  title: `General/Example`,
+  component: Example
+};
+
+export const Default = (): JSX.Element => <Example />;
