e4c2259cd28206ffde4af21eca914bb4abf10b5f
Fábio Maia <fabiomaia@suportes-MacBook-Pro.local>
2021-07-09 22:59:19 +0100

    Improved tests

--- src/packages/general/src/components/example/__tests__/example.tests.tsx
+++ src/packages/general/src/components/example/__tests__/example.tests.tsx
@@ -2,25 +2,9 @@ import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import { Example } from '../example';
 
-// describe('<AppRoot />', () => {
-// 	it('component should be defined and render', () => {
-// 		const wrapper = mount(
-// 			<AppRoot
-// 				globalData={{
-// 					company: Company.bcp,
-// 					language: Language.pt,
-// 					platform: Platform.mobile,
-// 				}}
-// 			/>
-// 		);
-
-// 		expect(wrapper.find(AppRoot)).toBeDefined();
-// 		expect(wrapper.find(AppRoot)).toHaveLength(1);
-// 	});
-// });
-
 test('default', async () => {
   render(<Example />);
 
   expect(screen).toBeDefined();
+  expect(screen.getByText('Example')).toBeDefined();
 });
